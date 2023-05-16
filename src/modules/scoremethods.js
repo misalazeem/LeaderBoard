9cecfc05b56b00e6eed3bb405e0970d931bca9f1
misalazeem <55094882+misalazeem@users.noreply.github.com>
2023-05-16 15:23:00 +0500

    adding all files

---
+++ src/modules/scoremethods.js
@@ -0,0 +1,13 @@
+import Score from './score.js'
+
+class ScoreList {
+    constructor(object = []){
+        this.scorelist = object;
+    }
+    
+    addScore(name, score) {
+      const add = new Score(name, score);
+      this.scorelist.push(add);
+    }
+};
+export default ScoreList;
