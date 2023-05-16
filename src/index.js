52f9c71be6b8b9631f707186d3a4e20ea8d191f6
misalazeem <55094882+misalazeem@users.noreply.github.com>
2023-05-16 15:36:42 +0500

    fix linters

--- src/index.js
+++ src/index.js
@@ -1,26 +1,26 @@
 import './style.css';
-import ScoreList from './modules/scoremethods.js'
+import ScoreList from './modules/scoremethods.js';
 
 const scorelist = new ScoreList();
 const score = document.querySelector('.table');
 function loadList() {
   score.innerHTML = '';
-  if (scorelist.scorelist.length != 0){
+  if (scorelist.scorelist.length !== 0) {
     scorelist.scorelist.forEach((score) => {
       const row = document.createElement('tr');
       const item = document.createElement('th');
-      item.value = score.name + ": " + score.score;
+      item.value = `${score.name}: ${score.score}`;
       row.appendChild(item);
       score.appendChild(row);
     });
-  }  
+  }
 }
 
 function loadSinglescore() {
   const index = scorelist.scorelist.length - 1;
   const row = document.createElement('tr');
   const item = document.createElement('th');
-  item.innerHTML = scorelist.scorelist[index].name + ": " + scorelist.scorelist[index].score;
+  item.innerHTML = `${scorelist.scorelist[index].name}: ${scorelist.scorelist[index].score}`;
   row.appendChild(item);
   score.appendChild(row);
 }
@@ -31,16 +31,15 @@ function onPageLoad() {
 
 document.addEventListener('DOMContentLoaded', () => {
   onPageLoad();
-  
+
   document.querySelector('#addscore').addEventListener('click', () => {
-    if ((document.querySelector('#scorename').value != '') && (document.querySelector('#yourscore').value != '')) {
-      const currentname = document.querySelector("#scorename").value;
-      const currentscore = document.querySelector("#yourscore").value;
+    if ((document.querySelector('#scorename').value !== '') && (document.querySelector('#yourscore').value !== '')) {
+      const currentname = document.querySelector('#scorename').value;
+      const currentscore = document.querySelector('#yourscore').value;
       scorelist.addScore(currentname, currentscore);
       loadSinglescore();
-      document.querySelector("#scorename").value = '';
-      document.querySelector("#yourscore").value = '';
+      document.querySelector('#scorename').value = '';
+      document.querySelector('#yourscore').value = '';
     }
   });
-  
 });
